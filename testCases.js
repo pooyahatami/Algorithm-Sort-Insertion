d25688b1addc1ddd04ec903367ed91de8837295f
pooyahatami <am.hatami@yahoo.com>
2017-06-25 11:58:44 -0700

    Update testCases.js

--- testCases.js
+++ testCases.js
@@ -2,14 +2,13 @@
  * Insertion sort algorithm !
  * Class	Sorting algorithm
  * Data structure	Array
+ * Category : Comparison sorts.
  * Worst-case performance	О(n2) comparisons, swaps
  * Best-case performance	O(n) comparisons, O(1) swaps
  * Average performance	О(n2) comparisons, swaps
  * Worst-case space complexity
  *   where n is the size of the input array.
- *   
  *   Note: Insertion sort is very similar to selection sort.
- *         Category : Comparison sorts.
  *
  * Author: Pooya Hatami
  */
